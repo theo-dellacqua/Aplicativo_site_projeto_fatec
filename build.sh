f80e9e3a8d9d1330a62ae1a3cd8c057754ca4bd1
theo <site@gmail.com>
2026-06-07 13:19:18 -0300

    arrumando erro do build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
- 
-echo "**********     início     **********"
- 
- 
-echo "**********  pip install   **********"
- 
-echo "********** makemigrations **********"
-python3 manage.py makemigrations --noinput
- 
-echo "**********    migrate     **********"
-python3 manage.py migrate --noinput
- 
-echo "********** collectstatic  **********"
-python3 manage.py collectstatic --noinput
- 
 
-echo "**********      fim       **********"
+echo "********** collectstatic **********"
+
+python manage.py collectstatic --noinput

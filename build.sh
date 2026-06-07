bd4f801e0a6b95408ed2a3d4ae02b9151aded0b3
theo <site@gmail.com>
2026-06-07 13:34:16 -0300

    mexendo no build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+ 
+echo "**********     início     **********"
+ 
+ 
+echo "**********  pip install   **********"
+python3 -m pip install -r requirements.txt
+ 
+echo "********** makemigrations **********"
+python3 manage.py makemigrations --noinput
+ 
+echo "**********    migrate     **********"
+python3 manage.py migrate --noinput
+ 
+echo "********** collectstatic  **********"
+python3 manage.py collectstatic --noinput
+ 
 
-echo "********** collectstatic **********"
-
-python manage.py collectstatic --noinput
+echo "**********      fim       **********"

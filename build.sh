7ca5ff092d39d550d89bdbc54e0745894ebeec96
theo <site@gmail.com>
2026-06-07 21:22:32 -0300

    arrumando o build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
- 
-echo "**********     início     **********"
- 
- 
-echo "**********  pip install   **********"
-python3 -m pip install -r requirements.txt
- 
-echo "********** makemigrations **********"
-python3 manage.py makemigrations --noinput
- 
-echo "**********    migrate     **********"
-python3 manage.py migrate --noinput
- 
-echo "********** collectstatic  **********"
-python3 manage.py collectstatic --noinput
- 
 
-echo "**********      fim       **********"
+echo "********** collectstatic **********"
+
+python manage.py collectstatic --noinput
+
+echo "********** fim **********"
